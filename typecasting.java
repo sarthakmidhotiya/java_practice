fafc69cd993571fefa9a4acefa55d65e81c3fe07
Sarthak Midhotiya <sarthakmidhotiya@gmail.com>
2026-02-24 01:14:08 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,17 @@
+// narrowing casting
+public class Main{
+    public static void main(String[] args){
+        double number = 87.98;
+        int result = (int) number;
+
+        System.out.println("double value" + number);
+        System.out.println("int value" + result);
+// widening casting
+        int number1 = 55;
+        double result2 = number1;
+
+        System.out.println("double value" + number1);
+        System.out.println("int value" + result1);        
+
+    }
+}
