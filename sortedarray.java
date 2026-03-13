bf16808ba4a45a88e593204cf193284e42968e40
Sarthak Midhotiya <sarthakmidhotiya@gmail.com>
2026-03-13 14:08:03 +0530

    Add files via upload

---
+++ sortedarray.java
@@ -0,0 +1,20 @@
+class Main{
+    public static void main(String[] args){
+        int[] arr = {10,4,2,6,5,9,8};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[j] > arr[j+1]){
+                int temp=0;
+                temp=arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                
+            }
+            }
+            
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
