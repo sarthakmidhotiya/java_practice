e0b6375e49ae1eac33afdfc1af7ca5f2a17105c3
Sarthak Midhotiya <sarthakmidhotiya@gmail.com>
2026-03-14 10:58:49 +0530

    Add files via upload

--- removeduplicateinarrayL.java
+++ removeduplicateinarrayL.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args){
-        int[] arr = {10,4,2,6,5,2,8};
+        int[] arr = {10,4,2,4,5,2,8};
         for(int i=0;i<arr.length;i++){
             for(int j=0; j<arr.length-1; j++){
                 if(arr[j] > arr[j+1]){
@@ -8,15 +8,16 @@ class Main{
                 temp=arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-                
             }
             }
-        }
-        for(int i=0; i<arr.length-1; i++){
-                arr[i] = arr[i+1];
-            }
-        for(int i=0; i<arr.length-1; i++){
-            System.out.println(arr[i]);
-        }
+       }
+            for(int i=0; i<arr.length-1; i++){
+               if(arr[i] != arr[i+1]){
+                   System.out.println(arr[i]);
+               }
+       }
+        
+            System.out.println(arr[arr.length-1]);
+        
         }
     }
