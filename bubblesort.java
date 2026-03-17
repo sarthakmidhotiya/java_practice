5cf23201964e1ed2092670915e7059b115893531
Sarthak Midhotiya <sarthakmidhotiya@gmail.com>
2026-03-17 15:48:47 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,6,2,5,1};
+        int temp = 0;
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
